6d0daf45b1c038dd8dba6983d561ffc3f34766d8
Md Sujan Mia <mdsujanmia860@gmail.com>
2024-07-13 02:28:13 +0600

    Easy Solution

--- LeetCode/762-Prime Number of Se Bits in Binary Representation.py
+++ LeetCode/762-Prime Number of Se Bits in Binary Representation.py
@@ -1,14 +1,15 @@
 # problem link - https://leetcode.com/problems/prime-number-of-set-bits-in-binary-representation/
 
-
-
 class Solution:
     def countPrimeSetBits(self, left: int, right: int) -> int:
-        primeNumber = [2, 3, 5, 7, 11, 13, 17, 19]       # A Maximum Binary bit of a Number is 1-19 bit. store 1 - 19 between prime Number 
+        # A Maximum Binary bit of a Number is 1-19 bit. store 1 - 19 between prime Number 
+        primeNumber = [2, 3, 5, 7, 11, 13, 17, 19]
         countPrime = 0
 
         for n in range(left, right+1):
-            countBit = bin(n).count("1")    #bin() for count 1 in a bianary number
+            #bin() for convert number in binary and coutn 1 in bit
+            countBit = bin(n).count("1")  
+
             if countBit in primeNumber:
                 countPrime += 1
         return countPrime
